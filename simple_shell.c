f3b57adeb5a0001f64c2c77865b1686326444b9a
Elvinooooo <Elvinooooo@github.com>
2023-12-26 06:55:26 -0800

    code adjusted

--- simple_shell.c
+++ simple_shell.c
@@ -16,26 +16,46 @@ int main(void)
 	while (1)
 	{
 		write(1, "#cisfun$ ", 9);
-		getline(&buffer, &buffer_size, stdin);
+		if (getline(&buffer, &buffer_size, stdin) == -1)
+		{
+			perror("getline");
+			break;
+		}
 		token = strtok(buffer, "\t\n");
 		memory = malloc(sizeof(char *) * 1024);
 		while (token != NULL)
 		{
-			memory[i] = token;
+			memory[i] = strdup(token);
 			token = strtok(NULL, "\t\n");
 			i++;
 		}
 		memory[i] = NULL;
-		free(memory);
+		free(buffer);
+		buffer = NULL;
+		buffer_size = 0;
 		child_pid = fork();
+		if (child_pid == -1)
+		{
+			perror("Fork");
+			exit(EXIT_FAILURE);
+		}
 		if (child_pid == 0)
 		{
 			if (execve(memory[0], memory, NULL) == -1)
+			{
 				perror("./shell: ");
+				exit(EXIT_FAILURE);
+			}
 		}
 		else
-			wait(&status);
+		{
+		wait(&status);
+		int j;
+		for (j = 0; j < i; j++)
+			free(memory[j]);
+		free(memory);
 		i = 0;
+		}
 	}
 	free(buffer);
 	return (0);
