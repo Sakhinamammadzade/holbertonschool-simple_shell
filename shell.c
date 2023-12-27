f53bade92447f78b3f31eda103f518079d77aeb3
Elvinooooo <Elvinooooo@github.com>
2023-12-27 11:15:10 -0800

    comments deleted

--- shell.c
+++ shell.c
@@ -16,7 +16,7 @@ int main(void)
     pid_t child_pid;
 	int j;
 
-    memory = malloc(sizeof(char *) * 1024);  // Move memory allocation outside the loop
+    memory = malloc(sizeof(char *) * 1024); 
 
     while (1)
     {
@@ -25,15 +25,15 @@ int main(void)
             break;
         }
 
-        // Reset i for each iteration
+
         i = 0;
 
-        // Tokenize with any whitespace character as delimiter
+
         token = strtok(buffer, " \t\n");
 
         while (token != NULL)
         {
-            memory[i] = malloc(strlen(token) + 1);  // Allocate memory for each token
+            memory[i] = malloc(strlen(token) + 1);
             strcpy(memory[i], token);
             token = strtok(NULL, " \t\n");
             i++;
@@ -47,14 +47,14 @@ int main(void)
             if (execve(memory[0], memory, NULL) == -1)
                 perror("ERROR execve:");
 
-            // This block should only be reached if execve fails
+
             exit(EXIT_FAILURE);
         }
         else
         {
             wait(&status);
 
-            // Free allocated memory in the parent process
+
             for (j = 0; j < i; j++)
             {
                 free(memory[j]);
