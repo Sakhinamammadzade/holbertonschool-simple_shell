cf88788a4ea50879e1dd110b00b67b9279f8bba9
Sakina Mammadzade <96643702+Sakhinamammadzade@users.noreply.github.com>
2023-12-27 20:06:41 +0400

    Update shell.c
    error

--- shell.c
+++ shell.c
@@ -1,3 +1,5 @@
+
+	
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -7,44 +9,50 @@
 
 int main(void)
 {
-	int status;
-	char *buffer = NULL;
-	size_t buffer_size = 0;
-	char *token;
-	int i = 0;
-	char **memory;
-	pid_t child_pid;
+    int status;
+    char *buffer = NULL;
+    size_t buffer_size = 0;
+    char *token, *saveptr;
+    char **memory;
+    pid_t child_pid;
 
-	while (1)
-	{
-		
-		if (getline(&buffer, &buffer_size, stdin) == -1)
-		{
-			printf("\n");
-			break;
-		}
-		
-		token = strtok(buffer, "\t\n");
-		memory = malloc(sizeof(char *) * 1024);
-		while (token != NULL)
-		{
-			memory[i] =  token;
-			token = strtok(NULL, "\t\n");
-			i++;
-		}
-		memory[i] = NULL;
-		child_pid = fork();
-		if (child_pid == 0)
-		{
-			if (execve(memory[0], memory, NULL) == -1)
-				perror("ERROR execvp:");
-                               
-		}
-		else
-			wait(&status);
-		i = 0;
-		free(memory);
-	}
-		free(buffer);
-	return (0);
+    while (1)
+    {
+        if (getline(&buffer, &buffer_size, stdin) == -1)
+        {
+            printf("\n");
+            break;
+        }
+
+        token = strtok_r(buffer, " \t\n", &saveptr);
+        memory = malloc(sizeof(char *));
+        int i = 0;
+        while (token != NULL)
+        {
+            memory = realloc(memory, (i + 1) * sizeof(char *));
+            memory[i] = strdup(token);
+            token = strtok_r(NULL, " \t\n", &saveptr);
+            i++;
+        }
+        memory[i] = NULL;
+
+        child_pid = fork();
+        if (child_pid == 0)
+        {
+            if (execvp(memory[0], memory) == -1)
+                perror("ERROR execvp:");
+            exit(EXIT_FAILURE);
+        }
+        else
+        {
+            wait(&status);
+            for (int j = 0; j <= i; j++)
+            {
+                free(memory[j]);
+            }
+            free(memory);
+        }
+    }
+    free(buffer);
+    return 0;
 }
