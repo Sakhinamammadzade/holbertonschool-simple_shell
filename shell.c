e3fbfb3933017495e9faf6d3ea41ebf0f503e012
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2023-12-29 02:06:19 -0800

    shell.c returned

--- shell.c
+++ shell.c
@@ -1,7 +1,5 @@
-#include "main.h"
-
+#include "shell.h"
 extern char **environ;
-
 int main(void)
 {
     int status;
@@ -10,24 +8,18 @@ int main(void)
     char *token;
     int i = 0;
     char **memory;
-    pid_t child_pid;
+pid_t child_pid;
     int j;
     int is_piped_input;
-
     memory = malloc(sizeof(char *) * 1024);
-
     while (1)
     {
         if (getline(&buffer, &buffer_size, stdin) == -1)
             break;
-
         i = 0;
-
         token = strtok(buffer, " \t\n");
-
         if (token == NULL)
             continue;
-
         while (token != NULL)
         {
             memory[i] = malloc(strlen(token) + 1);
@@ -35,14 +27,11 @@ int main(void)
             token = strtok(NULL, " \t\n");
             i++;
         }
-
         memory[i] = NULL;
-
         if (i > 0 && strcmp(memory[0], "exit") == 0)
         {
             for (j = 0; j < i; j++)
                 free(memory[j]);
-
             free(buffer);
             free(memory);
             exit(EXIT_SUCCESS);
@@ -59,67 +48,34 @@ int main(void)
         else
         {
             is_piped_input = isatty(fileno(stdin)) == 0;
-
-            // Check if the command exists in PATH
-            if (access(memory[0], X_OK) == 0)
+            child_pid = fork();
+            if (child_pid == 0)
             {
-                child_pid = fork();
-                if (child_pid == 0)
+                if (is_piped_input)
                 {
-                    if (is_piped_input)
+                    if (execvp(memory[0], memory) == -1)
                     {
-                        // Use popen to handle pipes
-                        FILE *pipe_fp = popen(buffer, "r");
-                        if (pipe_fp == NULL)
-                        {
-                            perror("ERROR popen:");
-                            exit(EXIT_FAILURE);
-                        }
-
-                        // Read from the pipe
-                        char line[1024];
-                        while (fgets(line, sizeof(line), pipe_fp) != NULL)
-                        {
-                            printf("%s", line);
-                        }
-
-                        // Close the pipe
-                        if (pclose(pipe_fp) == -1)
-                        {
-                            perror("ERROR pclose:");
-                            exit(EXIT_FAILURE);
-                        }
+                        perror("ERROR execvp:");
+                        exit(EXIT_FAILURE);
                     }
-                    else
-                    {
-                        if (execvp(memory[0], memory) == -1)
-                        {
-                            perror("ERROR execvp:");
-                            exit(EXIT_FAILURE);
-                        }
-                    }
-                }
-                else
+                } else
                 {
-                    wait(&status);
-                    if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
+                    if (execlp("/bin/sh", "sh", "-c", buffer, (char *)NULL) == -1)
                     {
-                        fprintf(stderr, "Command failed with exit status: %d\n", WEXITSTATUS(status));
+                        perror("ERROR execlp:");
+                        exit(EXIT_FAILURE);
                     }
                 }
             }
             else
             {
-                fprintf(stderr, "Command not found: %s\n", memory[0]);
+                wait(&status);
             }
         }
-
         for (j = 0; j < i; j++)
             free(memory[j]);
     }
-
     free(buffer);
     free(memory);
-
     return 0;
 }
