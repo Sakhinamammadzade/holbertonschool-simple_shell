5c464892f1e147f3f333ead0a62f9c7f940d7a75
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2023-12-29 13:53:30 -0800

    working with the double space

--- shell.c
+++ shell.c
@@ -1,6 +1,12 @@
-#include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 extern char **environ;
+
 int main(void)
 {
     int status;
@@ -9,18 +15,23 @@ int main(void)
     char *token;
     int i = 0;
     char **memory;
-pid_t child_pid;
+    pid_t child_pid;
     int j;
     int is_piped_input;
+
     memory = malloc(sizeof(char *) * 1024);
+
     while (1)
     {
         if (getline(&buffer, &buffer_size, stdin) == -1)
             break;
+
         i = 0;
         token = strtok(buffer, " \t\n");
+
         if (token == NULL)
             continue;
+
         while (token != NULL)
         {
             memory[i] = malloc(strlen(token) + 1);
@@ -28,11 +39,11 @@ pid_t child_pid;
             token = strtok(NULL, " \t\n");
             i++;
         }
+
         memory[i] = NULL;
+
         if (i > 0 && strcmp(memory[0], "exit") == 0)
         {
-            for (j = 0; j < i; j++)
-                free(memory[j]);
             free(buffer);
             free(memory);
             exit(EXIT_SUCCESS);
@@ -49,7 +60,8 @@ pid_t child_pid;
         else
         {
             is_piped_input = isatty(fileno(stdin)) == 0;
- 	    child_pid = fork();
+            child_pid = fork();
+
             if (child_pid == 0)
             {
                 if (is_piped_input)
@@ -57,42 +69,46 @@ pid_t child_pid;
                     if (execvp(memory[0], memory) == -1)
                     {
                         perror("ERROR execvp:");
-			fprintf(stderr, "./hsh: 1: %s: not found\n", memory[0]);
+                        fprintf(stderr, "./hsh: 1: %s: not found\n", memory[0]);
                         exit(EXIT_FAILURE);
                     }
-                } else
+                }
+                else
                 {
                     if (execvp(memory[0], memory) == -1)
                     {
                         perror("ERROR execvp:");
                         fprintf(stderr, "./hsh: 1: %s: not found\n", memory[0]);
                         exit(EXIT_FAILURE);
-                       
                     }
                 }
             }
             else
             {
                 wait(&status);
-		 if (WIFEXITED(status))
-        	{
-            	if (WEXITSTATUS(status) != 0)
-            	{
-                fprintf(stderr, "./hsh: 1: %s: not found\n", memory[0]);
-                exit(WEXITSTATUS(status));
-            	}
-        	}
-        	else if (WIFSIGNALED(status))
-        	{
-            fprintf(stderr, "./hsh: 1: %s: terminated by signal %d\n", memory[0], WTERMSIG(status));
-            exit(EXIT_FAILURE);
-        }
+
+                if (WIFEXITED(status))
+                {
+                    if (WEXITSTATUS(status) != 0)
+                    {
+                        fprintf(stderr, "./hsh: 1: %s: not found\n", memory[0]);
+                        exit(WEXITSTATUS(status));
+                    }
+                }
+                else if (WIFSIGNALED(status))
+                {
+                    fprintf(stderr, "./hsh: 1: %s: terminated by signal %d\n", memory[0], WTERMSIG(status));
+                    exit(EXIT_FAILURE);
+                }
             }
         }
+
         for (j = 0; j < i; j++)
             free(memory[j]);
     }
+
     free(buffer);
     free(memory);
     return 0;
 }
+
