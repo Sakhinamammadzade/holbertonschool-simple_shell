b095195203f63129fc145fd55a45bd9b061e9190
Elvinooooo <Elvinooooo@github.com>
2023-12-30 10:43:16 -0800

    shell.h file restored

---
+++ shell.h
@@ -0,0 +1,10 @@
+#ifndef SHELL_H
+#define SHELL_H
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#endif
